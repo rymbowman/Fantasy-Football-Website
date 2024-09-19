770ebee44ecd19ef6bb3b0bb411df7ed9eb129e7
Ryan Bowman <[email protected]>
2024-09-19 18:30:22 -0400

    Created VerseOfDay component that goes into the HomepageHeader component

--- src/components/HomepageHeader.jsx
+++ src/components/HomepageHeader.jsx
@@ -1,9 +1,10 @@
+import VerseOfDay from "./VerseOfDay";
+
 const HomepageHeader = () => {
   return (
     <div className="homepage-header">
       <h1 id="homepage-title">Flames Fantasy</h1>
-      <h4 id="verse-of-day"></h4>
-      <h6 id="verse-reference"></h6>
+      <VerseOfDay />
     </div>
   );
 };
---
+++ src/components/VerseOfDay.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const VerseOfDay = () => {
+  const [verse, setVerse] = useState("");
+  const [reference, setReference] = useState("");
+
+  useEffect(() => {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+    fetch(
+      "https://beta.ourmanna.com/api/v1/get?format=json&order=daily",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const verseText = data.verse.details.text;
+        const verseReference = data.verse.details.reference;
+        setVerse(verseText);
+        setReference(verseReference);
+      })
+      .catch((error) => {
+        console.error("error fetching verse", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <p id="verse-of-day">{verse}</p>
+      <p id="verse-reference">{reference}</p>
+    </div>
+  );
+};
+
+export default VerseOfDay;
