ff157f14f7b350d657a76874a4695376d18deb44
Ryan Bowman <[email protected]>
2024-11-25 16:29:02 -0500

    Updated Punishment component for styling purposes

--- src/components/punishments/Punishment.jsx
+++ src/components/punishments/Punishment.jsx
@@ -1,17 +1,40 @@
 import punishmentsData from "../../constants/data/punishmentsData";
 import PageHeader from "../pageHeaders/PageHeader";
 import "../punishments/Punishment.css";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import SlideshowImages from "./slideshowImages";
 
 const Punishment = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
   return (
     <div className="punishments-container">
       <PageHeader category={"punishments"} pageTitle={"League Punishments"} />
       {punishmentsData.map((punishment) => {
         return (
-          <div className={punishment.class} key={punishment.id}>
-            <img src={punishment.imgSrc} alt={punishment.imgAlt} />
-            <h3>{punishment.title}</h3>
-            <p>{punishment.description}</p>
+          <div
+            className={`${punishment.class} punishment-content`}
+            key={punishment.id}
+            data-aos="fade-in"
+          >
+            {punishment.slideshowImages ? (
+              <SlideshowImages images={punishment.slideshowImages} />
+            ) : (
+              <img
+                src={punishment.imgSrc}
+                alt={punishment.imgAlt}
+                className="punishment-img"
+              />
+            )}
+
+            <div className="punishment-text">
+              <h3 className="punishment-title">{punishment.title}</h3>
+              <p className="punishment-summary">{punishment.description}</p>
+            </div>
           </div>
         );
       })}
