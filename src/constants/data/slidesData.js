e28fa79ae9a1cf7933a9e846991ca3654d8de1e0
Ryan Bowman <[email protected]>
2024-12-07 19:22:27 -0500

    Updated images and captions in the homepage slideshow.

--- src/constants/data/slidesData.js
+++ src/constants/data/slidesData.js
@@ -1,33 +1,33 @@
 const headlinesImgsData = [
   {
     id: 1,
-    src: "https://images.unsplash.com/photo-1719937050814-72892488f741?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "This is a cutom caption",
+    src: "https://static.clubs.nfl.com/image/upload/t_editorial_landscape_12_desktop/eagles/ykuaweukrebjg2xqqexy",
+    caption:
+      "Can Carter capture his 3rd title after locking up the #1 seed? Led by Saquon Barkley, Carter's team could be poised for a deep playoff run.",
   },
   {
     id: 2,
-    src: "https://images.unsplash.com/photo-1723925089116-52652d744fc2?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "",
+    src: "https://library.sportingnews.com/styles/twitter_card_120x120/s3/2022-01/jamarr-chase-joe-burrow_1jm47pqz9y4yd1rsjrk884l1l6.jpg?itok=eElYypok",
+    caption:
+      "Carter's and Simon's teams earn first round byes after finishing the regular season as the number 1 and 2 seeds.",
   },
   {
     id: 3,
-    src: "https://images.unsplash.com/photo-1719937050814-72892488f741?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "",
+    src: "https://i0.wp.com/fffaceoff.com/wp-content/uploads/2023/07/losers.png?resize=735%2C414&ssl=1",
+    caption:
+      "Who will end up as the 2024 ultimate loser? Cole's and Kycob's teams lead the way for this unwanted title and fate.",
   },
   {
     id: 4,
-    src: "https://images.unsplash.com/photo-1719937050814-72892488f741?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "",
+    src: "https://library.sportingnews.com/styles/twitter_card_120x120/s3/2023-09/Garrett%20Wilson%20Breece%20Hall%20091223.png?itok=5FT0j7Sr",
+    caption:
+      "Big deadline trades shake up the league! Ryan, Simon, Miah, Josh, Carter, and Micah all make huge deals before the trade clock strikes midnight. Will their new additions be enough to push these teams into Flames Fantasy glory? Notable names on the move: Derrick Henry, Garrett Wilson, Baker Mayfield, Breece Hall and more!",
   },
   {
     id: 5,
-    src: "https://images.unsplash.com/photo-1719937050814-72892488f741?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "",
-  },
-  {
-    id: 6,
-    src: "https://images.unsplash.com/photo-1719937050814-72892488f741?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    caption: "",
+    src: "src/assets/images/jesse.jpeg",
+    caption:
+      "Team Jesse announces abrupt retirement following a difficult start to the season. Zach takes over for a franchise in turmoil. Can he right the ship?",
   },
 ];
 export default headlinesImgsData;
