f52222dd3ae4a21ff2e3ef0430b63d82162badd3
Ryan Bowman <rymbowman@gmail.com>
2024-12-20 13:29:15 -0500

    installed the latest rollup optional dependency and redownloaded npm

---
+++ dist/assets/PageHeader-Xrq80CK9.js
@@ -0,0 +1,1 @@
+import{P as e,j as r}from"./index-BpZPu7cu.js";const a=({pageTitle:s})=>r.jsx("div",{className:"header-div",children:r.jsx("h1",{className:"header-title",children:s})});a.propTypes={category:e.string,pageTitle:e.string};export{a as P};
--- dist/assets/PageHeader-Y3u8qxoU.js
+++
@@ -1,1 +0,0 @@
-import{P as e,j as r}from"./index-DnSvBsN8.js";const a=({pageTitle:s})=>r.jsx("div",{className:"header-div",children:r.jsx("h1",{className:"header-title",children:s})});a.propTypes={category:e.string,pageTitle:e.string};export{a as P};
