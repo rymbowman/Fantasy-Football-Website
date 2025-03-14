98f361c18dec7e12f7ccad78e4ff9813616654ca
Ryan Bowman <rymbowman@gmail.com>
2025-03-14 08:54:57 -0400

    Add useMemo and useEffect hooks to MatchupBoxScore for debugging stats data

--- src/components/previousChampions/MatchupBoxScore.jsx
+++ src/components/previousChampions/MatchupBoxScore.jsx
@@ -1,27 +1,46 @@
 import PropTypes from "prop-types";
+import { useEffect, useMemo } from "react";
 
 const MatchupBoxScore = ({ league }) => {
+  const championStats = useMemo(
+    () => league.champion.stats || [],
+    [league.champion.stats]
+  );
+  const runnerUpStats = useMemo(
+    () => league.runnerUp.stats || [],
+    [league.runnerUp.stats]
+  );
+
+  useEffect(() => {
+    console.log("Champion Stats:", championStats);
+    console.log("Runner-Up Stats:", runnerUpStats);
+  }, [championStats, runnerUpStats]);
+
   return (
     <>
       <div className="matchup-box-score">
         <ul className="team-stats-winner">
           <h4 className="boxscore-team-name">{league.champion.username}</h4>
-          {league.champion.stats.map((starter, index) => (
-            <div className="starter" key={index}>
-              <img
-                src={`https://sleepercdn.com/content/nfl/players/${starter.playerId}.jpg`}
-                alt={`image of ${starter.name}`}
-                className="starter-img"
-              />
-              <div className="starter-info starter-position">
-                {starter.position}
+          {championStats.length > 0 ? (
+            championStats.map((starter, index) => (
+              <div className="starter" key={index}>
+                <img
+                  src={`https://sleepercdn.com/content/nfl/players/${starter.playerId}.jpg`}
+                  alt={`image of ${starter.name}`}
+                  className="starter-img"
+                />
+                <div className="starter-info starter-position">
+                  {starter.position}
+                </div>
+                <div className="starter-info starter-name">{starter.name}</div>
+                <div className="starter-info starter-points">
+                  {starter.points}
+                </div>
               </div>
-              <div className="starter-info starter-name">{starter.name}</div>
-              <div className="starter-info starter-points">
-                {starter.points}
-              </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No stats available for champion</p>
+          )}
           <h4 className="boxscore-total-points-winner">
             {league.champion.points}
           </h4>
@@ -29,22 +48,26 @@ const MatchupBoxScore = ({ league }) => {
 
         <ul className="team-stats">
           <h4 className="boxscore-team-name">{league.runnerUp.username}</h4>
-          {league.runnerUp.stats.map((starter, index) => (
-            <div className="starter" key={index}>
-              <img
-                src={`https://sleepercdn.com/content/nfl/players/${starter.playerId}.jpg`}
-                alt={`image of ${starter.name}`}
-                className="starter-img"
-              />
-              <div className="starter-info starter-position">
-                {starter.position}
+          {runnerUpStats.length > 0 ? (
+            runnerUpStats.map((starter, index) => (
+              <div className="starter" key={index}>
+                <img
+                  src={`https://sleepercdn.com/content/nfl/players/${starter.playerId}.jpg`}
+                  alt={`image of ${starter.name}`}
+                  className="starter-img"
+                />
+                <div className="starter-info starter-position">
+                  {starter.position}
+                </div>
+                <div className="starter-info starter-name">{starter.name}</div>
+                <div className="starter-info starter-points">
+                  {starter.points}
+                </div>
               </div>
-              <div className="starter-info starter-name">{starter.name}</div>
-              <div className="starter-info starter-points">
-                {starter.points}
-              </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No stats available for runner-up</p>
+          )}
           <h4 className="boxscore-total-points">{league.runnerUp.points}</h4>
         </ul>
       </div>
@@ -64,7 +87,7 @@ MatchupBoxScore.propTypes = {
           position: PropTypes.string.isRequired,
           points: PropTypes.number.isRequired,
         })
-      ).isRequired,
+      ),
     }).isRequired,
     runnerUp: PropTypes.shape({
       username: PropTypes.string.isRequired,
@@ -76,7 +99,7 @@ MatchupBoxScore.propTypes = {
           position: PropTypes.string.isRequired,
           points: PropTypes.number.isRequired,
         })
-      ).isRequired,
+      ),
     }).isRequired,
   }).isRequired,
 };
